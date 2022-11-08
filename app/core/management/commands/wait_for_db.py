e00be846d5d761ba58e89cc8cb3b44de508b2349
ajith <ajith@metrictreelabs.com>
2022-11-08 11:09:53 +0530

    updates

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -13,14 +13,14 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """ entry point for comand """
-        self.stdout.write("waiting for database....")
+        self.stdout.write(self.style.WARNING("waiting for database...."))
         db_up = False
         while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
             except (Psycopg2OpError, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 second...')
+                self.stdout.write(self.style.NOTICE('Database unavailable, waiting 1 second...'))
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available..'))
